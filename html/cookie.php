d6351a40d94af7bb66e7f7aa4b05c19df67662c8
babylu <babylu910815@hotmail.com>
2015-12-05 00:28:11 -0500

    add item table

--- html/cookie.php
+++ html/cookie.php
@@ -33,8 +33,52 @@ and open the template in the editor.
             <center>
                 <fieldset form="iceCreamList">
                     <legend>Cookie</legend>
-                    <table>
-                        
+                    <div class="quantity">
+                        <label>Show</label> 
+                        <select id="showQuantity">
+                            <option value ="10">10</option>
+                            <option value ="20">20</option>
+                            <option value="30">30</option>
+                        </select>
+                        <label>Entities</label> 
+                    </div>
+                    <table class="itemShowTable">
+                        <!--when use php to generate this table, please ask Emily to do control of word length-->
+                            <tr>
+                                <th>Item Name</th>
+                                <th>Stock</th>
+                                <th>Price</th>
+                                <th>Buy Number</th>
+                            </tr>
+                            <tr>
+                                <td>Häagen-Dazs Vanilla</th>
+                                <td>100</th>
+                                <td>$5.99</th>
+                                <td>
+                                    <input type="text">
+                                    <button>Buy</button>
+                                </td>
+                                
+                            </tr>
+                            <tr>
+                                <td>Häagen-Dazs Pineapple and Coconut</th>
+                                <td>100</th>
+                                <td>$4.89</th>
+                                <td>
+                                    <input type="text">
+                                    <button>Buy</button>
+                                </td>
+                                
+                            </tr>
+                            <tr>
+                                <td>Häagen-Dazs Cherry</th>
+                                <td>100</th>
+                                <td>$5.89</th>
+                                <td>
+                                    <input type="text">
+                                    <button>Buy</button>
+                                </td>
+                            </tr>
                     </table>
                 </fieldset>
             </center>
