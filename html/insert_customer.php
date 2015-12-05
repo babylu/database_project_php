c1ba95959ede8f28f19e7329a370520dc0fbf3bf
lszlawrence <lszlawrence@gmail.com>
2015-12-04 20:33:25 -0500

    add php interaction

--- html/insert_customer.php
+++ html/insert_customer.php
@@ -11,13 +11,13 @@ if(!$con)
     die('could not connect:' . mysql_error);
 }
 mysql_select_db("e-commerce",$con);
-$sql="INSERT INTO customer (customer_id,name)
-    VALUES('$_POST[username]','$_POST[name]')";
+$sql="INSERT INTO customer (customer_id,name,address_street,address_city,address_state,address_zipcode,password)
+    VALUES('$_POST[username]','$_POST[name]','$_POST[address_street]','$_POST[address_city]','$_POST[address_state]','$_POST[address_zipcode]','$_POST[password]')";
    if (!mysql_query($sql,$con))
    {
        die ('error:' . mysql_error());
        
    }
-   echo "1 record added";
+   echo "<script>alert('register success')</script>";
    mysql_close($con)
    ?>
