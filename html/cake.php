4cddf4d6e26d2a2aeb64e43373491dc4086e3a6d
Ivy <Ivy@150.212.240.168>
2015-12-05 01:20:54 -0500

    Merge origin/master
    Conflicts:
    	html/register.html

---
+++ html/cake.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Ice Cream</title>
+<script type="text/javascript" src="../jquery-2.1.4.js"></script>
+<link rel="stylesheet" href="../css/common.css" type="text/css">
+<link rel="stylesheet" href="../css/pageIndex.css" type="text/css">
+</head>
+<body>
+    <div class="mainPart">
+        <div class="header">
+            <img class="title" alt="title" src="../img/title.jpg">
+            <div class="option">
+                <div id="username" class="username"><a href="logIn.html">Login/register</a></div>
+                <input id="searchInput" type="text" placeholder=" search"style='font-size:13px;'>
+                <img id="searchIcon" alt="searchIcon" src="../img/searchIcon20.jpeg">
+            </div>
+            <ul id="menu" class="menu">
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="iceCream.php">Ice Creams</a></li>
+                <li><a href="cake.php">Cakes</a></li>
+                <li><a href="chocolate.php">Chocolates</a></li>
+                <li><a href="cookie.php">Cookies</a></li>
+            </ul>
+        </div>
+        <div class="body">
+            cake
+        </div>
+
+        <div class="footer">
+            <div>Copyright &copy; Delicious Dessert Inc. All Right Reserved.</div>
+            <div>ADDRESS: </div>
+            <div>EMAIL: DeliciousDessertInc@gmail.com</div>
+            <div>PHONE: 412-***-****</div>
+        </div>
+    </div>
+    <?php
+
+    ?>
+</body>
+</html>
+
