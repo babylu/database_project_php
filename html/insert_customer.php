c22eacb08bcf97a0b49b531f9d55ff50b81e6d99
lszlawrence <lszlawrence@gmail.com>
2015-12-05 12:36:40 -0500

    add php

--- html/insert_customer.php
+++ html/insert_customer.php
@@ -5,19 +5,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$con = mysql_connect("localhost:8889","root","root");
+$con = mysqli_connect("localhost:8889","root","root","e-commerce");
 if(!$con)
 {
-    die('could not connect:' . mysql_error);
+    die('could not connect:' . mysqli_connect_error);
 }
-mysql_select_db("e-commerce",$con);
-$sql="INSERT INTO customer (customer_id,name,address_street,address_city,address_state,address_zipcode,password)
-    VALUES('$_POST[username]','$_POST[name]','$_POST[address_street]','$_POST[address_city]','$_POST[address_state]','$_POST[address_zipcode]','$_POST[password]')";
-   if (!mysql_query($sql,$con))
-   {
-       die ('error:' . mysql_error());
-       
-   }
-   echo "<script>alert('register success')</script>";
-   mysql_close($con)
+$sql="INSERT INTO customer (customer_id,name,address_street,address_city,address_state,address_zipcode,password,kind)
+    VALUES('$_POST[username]','$_POST[name]','$_POST[address_street]','$_POST[address_city]','$_POST[address_state]','$_POST[address_zipcode]','$_POST[password]','$_POST[type]');";
+$sql .="INSERT INTO personal (customer_id,marriage,gender,age,income)
+        VALUES('$_POST[username]','$_POST[marriageStatue]','$_POST[gender]','$_POST[age]','$_POST[income]')";
+
+ $sql1="INSERT INTO customer (customer_id,name,address_street,address_city,address_state,address_zipcode,password,kind)
+    VALUES('$_POST[username]','$_POST[name]','$_POST[address_street]','$_POST[address_city]','$_POST[address_state]','$_POST[address_zipcode]','$_POST[password]','$_POST[type]');";
+$sql1 .="INSERT INTO business (customer_id,category,gross_income)
+        VALUES('$_POST[username]','$_POST[category]','$_POST[gross_income]')";
+
+
+    if($_POST[type]==home){
+        if (mysqli_multi_query($con, $sql)) {  
+ echo "<script>alert('register success')</script>";     
+        ;      
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+}
+
+    }else {
+    
+    if (mysqli_multi_query($con, $sql1)) { 
+        echo "<script>alert('register success')</script>";
+          
+              
+} else {
+           echo  "Error: " . $sql1 . "<br>" . mysqli_error($con);
+}     
+}
+$con=null;
+  
    ?>
