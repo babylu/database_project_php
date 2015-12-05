cb216d0247d963226339ee3009fa2e6793bda213
babylu <babylu910815@hotmail.com>
2015-12-05 15:38:49 -0500

    add personal page

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
 $(function(){
     $('#username').click(function(){
-        $('#overlayContant').load('./html/loginDialog.html',function(){
-            $('#loginDialog').dialog({
-                modal: true,
-                height: 500,
-                width: 700,
-                zIndex: 5000,
-                closeOnEscape:true,
-                position: ['center','center'],
-                open: function () {
-                    $('.ui-dialog-titlebar-close').parent().hide();
-//                    $('.ui-dialog').unbind('focus');
-                }
-                
-            });
-            $('#loginClose').click(function(){
-                $('.ui-dialog').dialog('close');
-            });
-        });
+        var username = $(this).html();
+        if(username === "Login/Register"){
+            window.location.href = "http://localhost:8888/database_project_php/html/login.html";
+        }else{
+            window.location.href = "http://localhost:8888/database_project_php/html/personalPage.html";
+        }
     });
 });
 
