e59384706861ad2cab7d65da6c25a6b31b61e6be
lszlawrence <lszlawrence@gmail.com>
2015-12-06 15:32:51 -0500

    login

--- php/check.php
+++ php/check.php
@@ -5,29 +5,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-require_once ("get_data.php");
-$name=$_POST['customer_id'];
-$password=$_POST['password'];
-if($name == "")
-{
+    $db = mysql_connect("localhost:8889","root","root") or die("fail to connect database". mysql_error());
+    mysql_select_db("e-commerce")
+    or 
+    die ("can not connect to e-commerce".mysql_errno());
+    $sql = "select * from customer;";
+    $result = mysql_query($sql);
+    $column = mysql_fetch_array($result);
     
-    echo "<script type = 'text/javascript'>alert('please fill out your Email');location = 'http://localhost:8888/database_project_php/html/login.php'</script>";
-    
-}
-elseif($password=="")
-{
-    echo "<script type = 'text/javascript'>alert('please fill out your password');location = 'http://localhost:8888/database_project_php/html/login.php'</script>";
-}
-else
-{
-    $colum = get_data();
-    if(($colum['customer_id']==$name)&&($colum['password']==$password))
+    $name=$_POST['username'];
+    $password=$_POST['password'];
+     
+    if(($column['customer_id']==$name)&&($column['password']==$password))
     {
-       echo"<script type = 'text/javascript'>alert('login success!');location = 'http://localhost:8888/database_project_php/index.html'" ;
-    }
+        if($_POST['type'] == 'customer'){
+            echo "<script>alert('login success')</script>";
+            echo "<script>window.location.href = 'http://localhost:8888/database_project_php/index.html'</script>";
+           
+        }   
+          
     else
     {
-        echo "<script type = 'text/javascript'>alert('wrong password');location = 'http://localhost:8888/database_project_php/html/login.php'</script>";
+         echo "<script>alert('login success')</script>";
+         echo "<script>window.location.href = 'http://localhost:8888/database_project_php/html/adminDetail.php'</script>";
+        
     }
-}
+    }
 ?>
--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,4 @@
 
-
 <?php
 function(){
     $db = mysql_connect("localhost","root","root") or die("fail to connect database". mysql_error());
