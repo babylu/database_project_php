8fec1cda7a9961a05ea7c8c5265c7ba2b45619fc
Ivy <Ivy@150.212.240.168>
2015-12-05 21:23:08 -0500

    Merge origin/master
    Conflicts:
    	css/admin.css

--- html/adminEmployee.php
+++ html/adminEmployee.php
@@ -45,9 +45,9 @@ and open the template in the editor.
                     <input type="radio" name="EmpOption" id="add" value="add">Add
                     <input type="radio" name="EmpOption" id="modify" value="modify">Modify
                 </div>
-                <label></label>
+                
 <!--                show view employee part-->
-                <div id="viewEmp" >
+                <div id="viewOption" >
             <div class="search">
                     <label style="margin-left: 15px;">Find Employee information</label>
                                 <div class="searchEmp" >
@@ -86,7 +86,7 @@ and open the template in the editor.
                             </thead>
                             <tr>
                                 <td>Xiyi Li</td>
-                                <td>xil129@pitt.edu</td>
+                                <td>xil129</td>
                                 <td>3162 bohem street</td>
                                 <td>Pittsburgh</td>
                                 <td>PA</td>
@@ -110,7 +110,7 @@ and open the template in the editor.
                 </div>
                     </div>
 <!--                show add employee part-->
-<div id="addEmp" >
+<div id="addOption" >
     <div class="showTable">
                      
                          <center>
@@ -118,7 +118,7 @@ and open the template in the editor.
                     <table class="viewTable">
                         <tr>
                             <td>
-                                <label>Email:</label>           
+                                <label>Username:</label>           
                                 <input type="email" name="salesman_email" value=""></br>
                             </td>
                             <td>
@@ -154,8 +154,8 @@ and open the template in the editor.
                                 <input type="text" name="salary" value=""></br> 
                                     </td>
                                     <td>
-                                <label>Store ID:</label>
-                                    <select name="store_id">
+                                <label>Store Region:</label>
+                                    <select name="store_region" style="width:80px;">
                                         <option></option>
                                     </select>
                                     </td>
@@ -173,8 +173,79 @@ and open the template in the editor.
                 </div>
 </div>
 <!--                show modify employee part-->
-<div id="modifyEmp" >
-    test
+<div id="modifyOption" >
+    <div class="search">
+                    <label style="margin-left: 15px;">Find Employee information</label>
+                                <div class="searchEmp" >
+                                    <input type="text" name="employee_name" value="" placeholder=" Type employee name">
+                                    <button class="buttonStyle">Search </button>
+                                </div>
+            </div>
+    <div class="showForm">
+                    
+                    <center>
+                        <table class="viewTable">
+                            <form name="getemployee" action="editEmployee.php" method="POST">
+                            <thead>
+                                <tr>
+                                    <th >Name</th>
+                                    <th >Username</th>
+                                    <th >Street</th>
+                                    <th >City</th>
+                                    <th >State</th>
+                                    <th >Zip Code</th>
+                                   
+                                    <th >Salary</th>
+                                    <th >Job Title</th>
+                                    <th >Store</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tr>
+                                <td>
+                                    <label>xil129</label>
+                                </td>
+                                <td>
+                                    <input type="text" value="Ivy" style="width: 30px;">
+                                </td>
+                                
+                                <td>
+                                    <input type="text" value="3162 Bohem St" style="width: 80px;">
+                                </td>
+                                <td>
+                                    <input type="text" value="Pittsburgh" style="width: 60px;">
+                                </td>
+                                <td>
+                                    <input type="text" value="PA" style="width: 20px;">
+                                </td>
+                                <td>
+                                    <input type="text" value="15213" style="width: 40px;">
+                                </td>
+                             
+                                <td>
+                                    <input type="text" value="$34" style="width: 30px;">
+                                </td>
+                                <td>
+                                    <input type="text" value="Student" style="width: 50px;">
+                                </td>
+                                <td>
+                               
+                                    <select name="store_region" style="width:80px;">
+                                        <option value ="Pittsburgh Area">Pittsburgh Area</option>
+                                    </select>
+                                    </td>
+                                
+                                
+                            </tr>
+                            
+                        </table>
+                        <input type="submit" value="Save Changes" class="buttonStyle">
+                       
+                    </center>
+        
+                 </form>
+                    
+                </div>
 </div>
 </div>
         <div class="footer">
--- html/adminStock.php
+++ html/adminStock.php
@@ -8,8 +8,10 @@ and open the template in the editor.
 <head>
 <meta charset="UTF-8">
 <title>Hello World!</title>
+<script src="../js/jquery-1.9.0.min.js" type="text/javascript"></script>
 <link rel="stylesheet" href="../css/common.css" type="text/css">
 <link href="../css/admin.css" rel="stylesheet" type="text/css"/>
+<script src="../js/admin.js" type="text/javascript"></script>
 </head>
 <body>
     <div class="mainPart">
@@ -36,8 +38,15 @@ and open the template in the editor.
             </div>
             </div>
             <div class="right">
-                <div id="navHead" class="navHead" style="display:block">Inventory Information</div>
-            
+                <div id="navHead" class="navHead" style="display:block">
+                    <div id="navHead" class="navHead" style="display:block">
+                    <input type="radio" name="StoreOption" id="view" value="view" checked="">View
+                    <input type="radio" name="StoreOption" id="add" value="add">Add
+                    <input type="radio" name="StoreOption" id="modify" value="modify">Modify
+                </div>
+                </div>
+<!--        show view inventory part     -->
+<div id="viewOption">
             <div class="search">
                
    
@@ -71,7 +80,7 @@ and open the template in the editor.
                                     <th >Name</th>
                                     <th >Amount</th>
                                     <th >Price</th>
-                                    <th >Kind</th>
+                                    <th >Category</th>
                                     
                                     
                                 </tr>
@@ -92,6 +101,106 @@ and open the template in the editor.
                 
                     
                 </div>
+    </div>
+<!--            show add inventory part-->
+<div id="addOption">
+    <div class="showTable">                     
+                         <center>
+                <form name="addInventory" action="" method="POST">
+                    <div class="addform">
+                    <label>Category:</label>
+                    <select name="product_kind" style="width:80px;">
+                                        <option></option>
+                                    </select></br>
+                    </div>
+                    
+                    <input type="hidden" name="product_id" value="" />
+                    <div class="addform">
+                                <label>Product Name:</label>
+                                    <input type="text" name="InventoryName" value=""></br>
+                    </div>
+                    <div class="addform">
+                                <label>Product Price:</label>
+                                <input type="text" name="price" value=""></br>
+                    </div>
+                    <div class="addform>"
+                                <label>Add Amount:</label>
+                        <input type="text" name="amount" value="" style="width: 30px;height: 20px;"></br>
+                    </div>       
+                                
+                    </br>               
+                                
+                     <div class="addform">               
+                    <input type="submit" value="Add Inventory" class="buttonStyle">
+                     </div>           
+      
+                            
+                            
+                        </form>
+                             </center>
+                </div>
+</div>
+
+<!--                show modify inventory part-->
+<div id="modifyOption">
+    <div class="search">
+               
+   
+                <label style="margin-left: 15px;">Find product information</label>
+                                <div class="searchPro" >
+                                    <input type="text" name="product_name" value="" placeholder=" Type product name">
+                                    <button class="buttonStyle">Search </button>
+                                </div>
+                    
+             
+            </div>
+    <div class="showForm">
+                    
+                    
+                    
+                    <center>
+                        <table class="viewTable">
+                            
+                            <thead>
+                                <tr>
+                                    <th >ID</th>
+                                    <th >Name</th>
+                                    <th >Amount</th>
+                                    <th >Price</th>
+                                    <th >Category</th>
+                                    
+                                    
+                                </tr>
+                            </thead>
+                            <tr>
+                                <td>
+                                    <label>01</label>
+                                </td>
+                                <td>
+                                    <input type="text" value="Cheesecake" style="width: 80px;">
+                                </td>
+                                <td>
+                                    <input type="text" value="10" style="width: 30px;">
+                                </td>
+                                <td>
+                                    <input type="text" value="$8" style="width: 30px;">
+                                </td>
+                                <td>
+                                    <select name="product_category" style="width:80px;">
+                                        <option value="cake">Cake</option>
+                                    </select>
+                                </td>
+                                
+                                
+                            </tr>
+                          
+                        </table>
+                        <input type="submit" value="Save Changes" class="buttonStyle">
+                    </center>
+                
+                    
+                </div>
+</div>
 </div>
 
         <div class="footer">
