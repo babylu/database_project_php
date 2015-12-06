6eefd471392ebe6bff3e75cb8ad9d42cfaa354da
lszlawrence <lszlawrence@gmail.com>
2015-12-06 16:53:37 -0500

    login success

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(function(){
     $('#username').click(function(){
         var username = $(this).html();
         if(username === "Login/Register"){
-            window.location.href = "http://localhost:8888/database_project_php/html/login.html";
+            window.location.href = "http://localhost:8888/database_project_php/html/login.php";
         }else{
             window.location.href = "http://localhost:8888/database_project_php/html/personalPage.html";
         }
