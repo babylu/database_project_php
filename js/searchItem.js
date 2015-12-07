0c4f4bf8235784c4385956f1bfff330056701ff4
babylu <babylu910815@hotmail.com>
2015-12-06 22:52:47 -0500

    add searchItem.js

---
+++ js/searchItem.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function(){
+    $('#searchIcon').click(function(){
+        var searchInfo = $('#searchInput').val();
+        window.location.href='http://localhost:8888/database_project_php/html/searchResult.php'+'?'+searchInfo;
+    });
+});
+
